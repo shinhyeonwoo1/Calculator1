7511a4c9b30d5f7c27d1fc83f3f4b83bfa6f2129
shinhyeonwoo <shinhu2016a@gmail.com>
2025-04-17 19:34:30 +0900

    if change

--- src/creative/Main.java
+++ src/creative/Main.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("첫번째 숫자를 입력하세요:");
-        int a = scanner.nextByte();
-        System.out.println("두번째 숫자를 입력하세요:");
-        double b = scanner.nextByte();
+//        System.out.println("정수 1:");
+//        int a = scanner.nextByte();
+//        System.out.println("정수 2:");
+//        double b = scanner.nextDouble();
+//
+//        double div = a / b;
+//        System.out.printf("결과값:" + div);
+//        System.out.println("나이를 입력하세요:");
+//        int age = scanner.nextByte();
+//
+//        if (age < 20) {
+//            System.out.println("미성년자입니다.");
+//        } else if (age > 20) {
+//            System.out.println("성인입니다.");
+//
+//        }
+//
+//
+//    }
 
-        double div = a / b;
-        System.out.println("결과값:" + div);
 
-
-    }
 }
+
