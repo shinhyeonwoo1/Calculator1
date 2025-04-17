54a8a29f00351ff86c35d6edbf69e2017c89d861
shinhyeonwoo <shinhu2016a@gmail.com>
2025-04-17 21:28:39 +0900

    for changes

--- src/creative/Main.java
+++ src/creative/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //연산자
 //        System.out.println("정수 1:");
 //        int a = scanner.nextByte();
 //        System.out.println("정수 2:");
@@ -12,12 +13,14 @@ public class Main {
 //
 //        double div = a / b;
 //        System.out.printf("결과값:" + div);
+
+        //조건문
 //        System.out.println("나이를 입력하세요:");
 //        int age = scanner.nextByte();
 //
 //        if (age < 20) {
 //            System.out.println("미성년자입니다.");
-//        } else if (age > 20) {
+//        } else if (age => 20) {
 //            System.out.println("성인입니다.");
 //
 //        }
@@ -25,6 +28,22 @@ public class Main {
 //
 //    }
 
+        System.out.println("비트개수:");
 
-}
+
+        int money = scanner.nextByte();
+
+
+        for (int a = 1; a <= 5; a++) {
+            System.out.println("비트개수:" + (money + money++));
+            }
+
+            }
+
+
+        }
+
+
+
+
 
